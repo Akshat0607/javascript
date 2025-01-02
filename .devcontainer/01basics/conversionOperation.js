b3d97bc36921c722bce07ecb914529f9ece5bbcb
Akshat0607 <128149486+Akshat0607@users.noreply.github.com>
2025-01-02 17:52:02 +0000

    operations

--- .devcontainer/01basics/conversionOperation.js
+++ .devcontainer/01basics/conversionOperation.js
@@ -1,15 +1,40 @@
-let score="33"
-console.log(typeof score);
-console.log(typeof (score));
+// let score="33"
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
-//"33" =.33
-//"33abc"=>NaN 
+// //"33" =.33
+// //"33abc"=>NaN 
 
-let isLoggedIn="abc"
+// let isLoggedIn="abc"
 
-let booleanIsLoggedIn=Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+ //*********************************operations************************* */
+
+
+//  let value=3
+//  let negvalue=-value
+//  console.log(negvalue);
+ 
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**2);
+//  console.log(2/2);
+//  console.log(2%2);
+
+let str1="aj "
+let str2="hello bro"
+let str3=str1+str2;
+console.log(str3)
+ 
+
+let gameCounter=100;
+++gameCounter;
+gameCounter++;
+console.log(gameCounter);
