a77ec8d91f2a797aa44a730855d538837fa266be
Akshat0607 <128149486+Akshat0607@users.noreply.github.com>
2025-01-02 17:26:47 +0000

    variables and datattypes

---
+++ .devcontainer/01basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=123;
+let accountEmail="akshatjain9310@gmail.com";
+var accountPassword="1234";
+accountCity="jaipur";
+let accountState;
+// accountId=234;// not allowed 
+
+
+accountEmail="abc@gmail.com";
+accountPassword="12345";
+accountCity="viajypur"
+// console.log(accountEmail);
+console.table([accountEmail,accountPassword,accountCity,accountState]);
+
+/*
+prefer not to use var cpz oof issue in block scoupe and functional scope
+*/
---
+++ .devcontainer/01basics/02_DataTypes.js
@@ -0,0 +1,13 @@
+ //code readablitity should be high  
+ let name="akshat"
+ let age=null
+ let isLoggedIn=true
+ //number
+ //bigin
+ //string=.""
+ //boolean
+ //null //object
+//undefined //undefined 
+//symbol
+
+console.log(typeof age)
