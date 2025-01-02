e05441598d24bf74cad0271578e9d64796d22420
Akshat0607 <128149486+Akshat0607@users.noreply.github.com>
2025-01-02 17:39:38 +0000

    operations

---
+++ .devcontainer/01basics/conversionOperation.js
@@ -0,0 +1,15 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" =.33
+//"33abc"=>NaN 
+
+let isLoggedIn="abc"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
